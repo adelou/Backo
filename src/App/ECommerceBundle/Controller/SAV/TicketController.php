78f1124aba828bfd844e62957f78b05bf8bf7bc2
Adel Oustad <a.oustad@novediagroup.com>
2014-10-30 22:25:46 +0100

    no message

--- src/App/ECommerceBundle/Controller/SAV/TicketController.php
+++ src/App/ECommerceBundle/Controller/SAV/TicketController.php
@@ -325,7 +325,7 @@ class TicketController extends Controller
      * @Method("GET")
      * @Template("AppECommerceBundle:SAV\Ticket:newMessage.html.twig")
      */
-    protected function newMessageAction($id) {
+    public function newMessageAction($id) {
         $em = $this->getDoctrine()->getManager();
         $ticket = $em->getRepository('AppECommerceBundle:SAV\Ticket')->find($id);
 
